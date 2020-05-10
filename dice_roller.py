79839265cce89f56cfca354432f9529589e52c9e
Aravind Sankar <aravinds1996@gmail.com>
2020-05-10 18:23:13 -0400

    User Input Dice

--- dice_roller.py
+++ dice_roller.py
@@ -1,14 +1,14 @@
 import random
 
 def main():
-  number_dice =2
+  dice_size =int(input('How many dice would you like to throw?'))
   dice_sum =0
-  for i in range(0,number_dice):  
+  for i in range(0,dice_size):  
     roll=random.randint(1,6)
     dice_sum+=roll
     if(roll==1):
       print(f'You rolled a {roll}! Critical Fail!')
-    elif(roll==6):
+    elif(roll==dice_size):
       print(f'You rolled a {roll}! Critical Sucess!')
     else: 
       print(f'You rolled a {roll}')
